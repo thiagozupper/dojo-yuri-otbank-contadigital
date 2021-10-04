19bc77107baca7de08f5bddb301aef4fc8b63f6c
raifeDev <raife.paiva@zup.com.br>
2021-10-04 14:57:05 -0300

    Projeto base

---
+++ src/test/java/br/com/contadigital/ContaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.contadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
